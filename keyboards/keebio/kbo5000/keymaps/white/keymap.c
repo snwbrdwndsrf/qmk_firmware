e30d55c10bc0e10346c9da5f4eaf006b87a8cf86
Simon Hall <sihall@google.com>
2023-02-26 11:14:51 -0800

    .

--- keyboards/keebio/kbo5000/keymaps/white/keymap.c
+++ keyboards/keebio/kbo5000/keymaps/white/keymap.c
@@ -1,10 +1,11 @@
 #include QMK_KEYBOARD_H
 
-#define MAC_OS
-//#define LINUX
+//#define MAC_OS
+#define LINUX
 
 // Define as many things as possible as common between MacOS and Linux.
-#ifdef MAC_OS
+#if defined(MAC_OS)
+  #define SS_END SS_DOWN(X_LGUI) SS_TAP(X_RIGHT) SS_UP(X_LGUI)
   #define ALT_TAB_MOD KC_LGUI
   #define SH_WINCL G(KC_W)
   #define SH_HOME G(KC_LEFT)
@@ -15,9 +16,10 @@
   #define SH_PSTE G(KC_V)
   #define SH_UNDO G(KC_Z)
   #define SH_NWTB G(KC_T)
-#endif
-
-#ifdef LINUX
+  #define SH_WORDL _______
+  #define SH_WORDR _______
+#elif defined(LINUX)
+  #define SS_END SS_TAP(X_END)
   #define ALT_TAB_MOD KC_LALT
   #define SH_WINCL C(KC_W)
   #define SH_HOME KC_HOME
@@ -27,6 +29,8 @@
   #define SH_PSTE C(KC_V)
   #define SH_UNDO C(KC_Z)
   #define SH_NWTB C(KC_T)
+  #define SH_WORDL C(KC_LEFT)
+  #define SH_WORDR C(KC_RIGHT)
 #endif
 
 // CapsLock on tap, CTRL on hold.
@@ -37,7 +41,40 @@ enum my_keycodes {
   SH_NEW_LINE = SAFE_RANGE,
   SH_NL = SH_NEW_LINE,
   SH_PWD,
+  SH_TAB,
 };
+
+// Custom tabbing mode variables.
+#define TABBING_TIMEOUT 750
+#define SH_TABBING_APPS ALT_TAB_MOD
+#define SH_TABBING_TABS KC_LCTL
+static struct {
+  bool tabbing;
+  uint16_t tabtimer;
+  uint16_t tab_unregister_code;
+} tabbing_state;
+
+void TabNext(void) {
+  if (!tabbing_state.tabbing) {
+    register_code(tabbing_state.tab_unregister_code =
+                      (IS_LAYER_ON(1) ? SH_TABBING_TABS : SH_TABBING_APPS));
+    tabbing_state.tabbing = true;
+  }
+  tabbing_state.tabtimer = timer_read();
+  tap_code(KC_TAB);
+}
+
+void TabPrev(void) {
+  if (!tabbing_state.tabbing) {
+    register_code(tabbing_state.tab_unregister_code =
+                      (IS_LAYER_ON(1) ? SH_TABBING_TABS : SH_TABBING_APPS));
+    tabbing_state.tabbing = true;
+  }
+  tabbing_state.tabtimer = timer_read();
+  tap_code16(S(KC_TAB));
+}
+
+//---------------------------------------------------------------------------------
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ansi(
@@ -52,14 +89,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_ansi(
     _______,          RGB_VAI, RGB_VAD, RGB_SAI, RGB_SAD, RGB_HUI, RGB_HUD,          RGB_M_P, RGB_M_B,          RGB_M_R,RGB_M_SW,RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T,
     _______, RGB_MOD, BL_STEP, RGB_TOG, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, KC_DEL,  KC_DEL,  _______, _______,
-    _______, _______, _______, SH_WINCL,SH_END,  KC_PGUP, SH_NWTB,                   _______, SH_UNDO, SH_HOME, SH_NL,   _______, _______, _______, _______, _______, _______,
-    DM_RSTP, KC_CAPS, _______, _______, _______, KC_PGDN, _______,                   KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______, _______,          _______, _______, _______,
-    DM_REC1, _______,          _______, SH_CUT,  SH_COPY, SH_PSTE, _______,          _______, _______, _______, _______, _______,          _______,          _______,
+    _______,  SH_TAB, _______, SH_WINCL,SH_END,  KC_PGUP, SH_NWTB,                   _______, SH_UNDO, SH_HOME, SH_NL,   _______, _______, _______, _______, _______, _______,
+    DM_RSTP, KC_CAPS, _______, C(KC_S), _______, KC_PGDN, _______,                   KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______, _______,          _______, _______, _______,
+    DM_REC1, _______,          _______, SH_CUT,  SH_COPY, SH_PSTE, _______,          SH_WORDL,SH_WORDR,_______, _______, _______,          _______,          _______,
     DM_REC2, _______, _______, _______, _______, _______, _______,                   _______, KC_MINS, _______, _______,                   _______, _______, _______, _______
   ),
 
   [2] = LAYOUT_ansi(
-    _______,          _______, _______, _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______, _______, SH_PWD,  _______,
+    QK_BOOT,          _______, _______, _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______, _______, SH_PWD,  _______,
     _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, DT_PRNT, DT_DOWN, DT_UP,   _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, CL_TOGG, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,          _______, _______, _______,
@@ -68,11 +105,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-#ifdef MAC_OS
- #define SS_END SS_DOWN(X_LGUI) SS_TAP(X_RIGHT) SS_UP(X_LGUI)
-#else
- #define SS_END SS_TAP(X_END)
-#endif
+//---------------------------------------------------------------------------------
+// Custom keycode handling (for custom keycodes and custom behaviors).
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   uint16_t mods = get_mods();
@@ -91,47 +125,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case SH_PWD:
       if (record->event.pressed) SEND_STRING(";;l4g3k8lo\n");
       return false;
+    case SH_TAB:
+      if (record->event.pressed && IS_LAYER_ON(1)) {
+        mods & MOD_MASK_SHIFT ? TabPrev() : TabNext();
+        return false;
+      }
+      return true;
     default:
-      return true; // Process all other keycodes normally
+      return true;  // Process all other keycodes normally
   }
   return true;
 }
 
-
+//---------------------------------------------------------------------------------
 // Rotary encoder
+
 enum encoder_names {
   LEFT_HALF_ENC = 0,
   RIGHT_HALF_ENC1 = 2,
   RIGHT_HALF_ENC2,
 };
 
-#define TABBING_TIMEOUT 750
-#define SH_TABBING_APPS ALT_TAB_MOD
-#define SH_TABBING_TABS KC_LCTL
-static bool tabbing = false;
-static uint16_t tabtimer;
-static uint16_t tab_unregister_code;
-
 bool encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == RIGHT_HALF_ENC2) {
-    if(!tabbing) {
-      register_code(tab_unregister_code =
-          (IS_LAYER_ON(1) ? SH_TABBING_TABS
-                                : SH_TABBING_APPS));
-      tabbing = true;
-    }
-    tabtimer = timer_read();
-    tap_code16(clockwise ? KC_TAB : S(KC_TAB));
-  }
+  if (index == RIGHT_HALF_ENC2) clockwise ? TabNext() : TabPrev();
   return false;
 }
 
 void matrix_scan_user(void) {
-  if(tabbing) {
-    if (timer_elapsed(tabtimer) > TABBING_TIMEOUT) {
-      unregister_code(tab_unregister_code);
-      tabbing = false;
-    }
+  if (tabbing_state.tabbing && timer_elapsed(tabbing_state.tabtimer) > TABBING_TIMEOUT) {
+    unregister_code(tabbing_state.tab_unregister_code);
+    tabbing_state.tabbing = false;
   }
 }
 
@@ -159,32 +182,36 @@ const uint16_t PROGMEM return_if_error_combo[] = {KC_R, KC_I, KC_E, COMBO_END};
 const uint16_t PROGMEM assign_or_return_combo[] = {KC_A, KC_O, KC_R, COMBO_END};
 
 combo_t key_combos[] = {
-  [CC_IF] = COMBO_ACTION(if_combo),
-  [CC_ELSE] = COMBO_ACTION(else_combo),
-  [CC_ELIF] = COMBO_ACTION(elif_combo),
-  [CC_FOR] = COMBO_ACTION(for_combo),
-  [CC_WHILE] = COMBO_ACTION(while_combo),
-  [CC_RETURN_IF_ERROR] = COMBO_ACTION(return_if_error_combo),
-  [CC_ASSIGN_OR_RETURN] = COMBO_ACTION(assign_or_return_combo),
+    [CC_IF] = COMBO_ACTION(if_combo),
+    [CC_ELSE] = COMBO_ACTION(else_combo),
+    [CC_ELIF] = COMBO_ACTION(elif_combo),
+    [CC_FOR] = COMBO_ACTION(for_combo),
+    [CC_WHILE] = COMBO_ACTION(while_combo),
+    [CC_RETURN_IF_ERROR] = COMBO_ACTION(return_if_error_combo),
+    [CC_ASSIGN_OR_RETURN] = COMBO_ACTION(assign_or_return_combo),
 };
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
   if (pressed) {
-    switch(combo_index) {
+    switch (combo_index) {
       case CC_IF:
-        SEND_STRING("if () {}" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT));
+        SEND_STRING("if () {}" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT)
+                        SS_TAP(X_LEFT));
         break;
       case CC_ELSE:
         SEND_STRING("else {}" SS_TAP(X_LEFT));
         break;
       case CC_ELIF:
-        SEND_STRING("else if () {}" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT));
+        SEND_STRING("else if () {}" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT)
+                        SS_TAP(X_LEFT));
         break;
       case CC_FOR:
-        SEND_STRING("for () {}" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT));
+        SEND_STRING("for () {}" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT)
+                        SS_TAP(X_LEFT));
         break;
       case CC_WHILE:
-        SEND_STRING("while () {}" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT));
+        SEND_STRING("while () {}" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT)
+                        SS_TAP(X_LEFT));
         break;
       case CC_RETURN_IF_ERROR:
         SEND_STRING("RETURN_IF_ERROR();" SS_TAP(X_LEFT) SS_TAP(X_LEFT));
@@ -195,4 +222,3 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
     }
   }
 }
-
